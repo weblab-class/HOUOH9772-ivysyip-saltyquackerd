7599fee21493665a134db2fa3fe46a83270fb539
Ivy Yip <ivysyip@gmail.com>
2025-01-16 15:26:51 -0500

    welcome page just not center sad :(

---
+++ client/src/components/pages/Welcome.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import "./Welcome.css";
+import { UserContext } from "../App";
+
+const Welcome = () => {
+  const { handleLogin } = useContext(UserContext);
+  return (
+    <div className="u-welcome">
+      <div className="welcome-content">
+        <h1 className="welcome-title">Welcome to picVenture!</h1>
+        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
