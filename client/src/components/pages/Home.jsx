bbf99b724520c2e3832693fbc320bfb629cbba68
Carol Li <carol6689@dhcp-10-29-214-89.dyn.MIT.EDU>
2025-01-19 00:03:03 -0500

    homepage first draft

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -7,10 +7,18 @@ import { UserContext } from "../App";
 const Home = () => {
   const { userId, handleLogin, handleLogout } = useContext(UserContext);
   return (
-    <>
-      <div className="u-homepage">
-        <h1 className="home-title">Today's Challenge</h1>
+    <div className="u-homepage">
+      {/* Insert below the challenge */}
+      <h1 className="home-title">Today's Challenge</h1>
+      <div className="challenge-upload">
+        <div className="upload">
+          <form action="/action_page.php">
+            <input type="file" id="myFile" name="filename" />
+            <input type="submit" />
+          </form>
+        </div>
       </div>
-    </>
-  )};
+      <h1 className="challenge">wear something blue</h1>
+    </di
+  );};
 export default Home;
