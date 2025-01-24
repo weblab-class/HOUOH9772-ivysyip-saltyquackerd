c565b10ddfca7cbb2440963f8e0c8062b0b1f5ad
Ivy Yip <ivysyip@gmail.com>
2025-01-24 03:23:10 -0500

    change to utc timing and stored in desired format

--- client/src/components/pages/Friends.jsx
+++ client/src/components/pages/Friends.jsx
@@ -9,7 +9,8 @@ import "./Friends.css";
 const Friends = () => {
   const { userId } = useContext(UserContext);
   const [user, setUser] = useState(null);
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date(Date.now()));
+  const [filteredDate, setFilteredDate] = useState("");
 
   useEffect(() => {
     if (userId) {
@@ -17,19 +18,16 @@ const Friends = () => {
         setUser(user);
       });
     }
+    setFilteredDate(selectedDate.toISOString().split("T")[0]);
   }, []);
+
+  useEffect(() => {
+    setFilteredDate(selectedDate.toISOString().split("T")[0]);
+  }, [selectedDate]);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-
-  // let groupsList = null;
-  // const hasGroups = groups.length !== 0;
-  // if (hasGroups) {
-  //   groupsList = groups.map((groupObj) => <p>{groupObj.group_name}</p>);
-  // } else {
-  //   groupsList = <div>No groups! Create a group!</div>;
-  // }
 
   return (
     <>
@@ -37,9 +35,9 @@ const Friends = () => {
         <>Please login</>
       ) : (
         <div className="container">
-          {/* <button onClick={() => setCreateGroup(true)}>Create/Join Group</button> */}
-          <Calendar onChange={handleDateChange} value={selectedDate} />
-          {/* {groupsList} */}
+          <div className="calender-container">
+            <Calendar onChange={handleDateChange} value={selectedDate} />
+          </div>
           <Tabs />
         </div>
       )}
